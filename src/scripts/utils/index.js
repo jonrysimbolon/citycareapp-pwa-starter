5df0371113759f41b0ca9221c251383527d33aa3
jonry <jonrysimbolon19@gmail.com>
2025-06-13 10:29:05 +0700

    remove-2 docs

--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -105,7 +105,7 @@ export async function registerServiceWorker() {
   }
 
   try {
-    const registration = await navigator.serviceWorker.register('/sw.bundle.js');
+    const registration = await navigator.serviceWorker.register('/citycareapp-pwa-starter/sw.bundle.js'); // untuk case github page, harus sesuai url
     console.log('Service worker telah terpasang', registration);
   } catch (error) {
     console.error('Failed to install service worker:', error);
